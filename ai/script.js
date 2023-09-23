a76eea63394b546c1c8231ad543967b320ffb45b
Sajid <sajid@duck.com>
2023-09-23 22:51:20 +0530

    font & animation

--- ai/script.js
+++ ai/script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
-    const loader = document.querySelector(".line-wobble");
+    const loader = document.querySelector(".loader-container");
     const h1 = document.querySelector("h1");
 
-    
     setTimeout(function() {
-        loader.style.display = "none"; 
-        h1.style.opacity = 1; 
-    }, 3000); 
+        loader.style.display = "none";
+
+        
+        setTimeout(function() {
+            h1.style.opacity = 1;
+        }, 1000); 
+    }, 5000); 
 });
